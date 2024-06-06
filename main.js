c17bd8015d1c65d3446b15161bf50fb524500aa6
Abdelrahman Elrayes <abdelrahmanelrayes2@gmail.com>
2024-06-07 00:11:46 +0300

    dark theme

--- main.js
+++ main.js
@@ -1,16 +1,67 @@
 'use strict';
 
+const BACKGROUND = '#1a2c47';
+const OPERATIONCOLOR = '#61738e';
+const ENTERBTN = '#bacbd8';
+const OVERLAY = '#0e1e38e0';
+
 let themeBack = document.querySelector('.in-theme-btn');
 let screen = document.querySelector('.screen');
 let delBtn = document.querySelector('.btn-del');
 let acBtn = document.querySelector('.btn-ac');
 let enterBtn = document.querySelector('.btn-enter');
 let ansBtn = document.querySelector('.btn-ans');
-
-themeBack.addEventListener('click', () => {
-  themeBack.classList.toggle('right-theme');
-});
 let btns = document.querySelectorAll('.btn');
+
+let darkOverlay = document.querySelector('.container');
+let darkBtnBackground = document.querySelector('.btns');
+let headerBackground = document.querySelector('.header');
+let opsBtnBackground = document.querySelectorAll('.btn-ops');
+function changeOperationBtnNew() {
+  opsBtnBackground.forEach(e => {
+    e.style.backgroundColor = OPERATIONCOLOR;
+  });
+}
+function changeBtnFontColorNew() {
+  btns.forEach(e => {
+    if (e.classList.contains('btn-co')) e.style.color = BACKGROUND;
+  });
+}
+function changeOperationBtnOld() {
+  opsBtnBackground.forEach(e => {
+    e.style.backgroundColor = '#00b7bd';
+  });
+}
+function changeBtnFontColorOld() {
+  btns.forEach(e => {
+    if (e.classList.contains('btn-co')) e.style.color = '#005866';
+  });
+}
+themeBack.addEventListener('click', () => {
+  // themeBack.classList.toggle('right-theme');
+  if (!themeBack.classList.contains('right-theme')) {
+    themeBack.classList.add('right-theme');
+    enterBtn.style.backgroundColor = ENTERBTN;
+    screen.style.backgroundColor = BACKGROUND;
+    darkBtnBackground.style.backgroundColor = BACKGROUND;
+    themeBack.style.backgroundColor = BACKGROUND;
+    darkOverlay.style.background = OVERLAY;
+    headerBackground.style.color = BACKGROUND;
+    changeOperationBtnNew();
+    changeBtnFontColorNew();
+  } else {
+    themeBack.classList.remove('right-theme');
+    enterBtn.style.backgroundColor = '#b8f5ee';
+    screen.style.backgroundColor = '#005866';
+    darkBtnBackground.style.backgroundColor = '#005866';
+    themeBack.style.backgroundColor = '#005866';
+    darkOverlay.style.background = '#8aeef0a6';
+    headerBackground.style.color = '#005866';
+    changeOperationBtnOld();
+    changeBtnFontColorOld();
+  }
+});
+
 let btnPressed = [
   0,
   1,
